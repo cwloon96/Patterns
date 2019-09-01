b44d9d3d40b6fd716bb3f92f346fc5af58f16c46
Woei Loon Choo <woeiloon96@gmail.com>
2019-09-01 17:12:13 +0800

    proxy example

---
+++ PatternsExamples/Structural/Proxy/OrderSystems.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsExamples.Structural.Proxy
+{
+    public interface IOrderSystem
+    {
+        bool MakeOrder();
+        bool MakePayment();
+    }
+
+    public class OrderSystem : IOrderSystem
+    {
+        public bool MakeOrder()
+        {
+            Console.WriteLine("Process Order");
+            return true;
+        }
+
+        public bool MakePayment()
+        {
+            Console.WriteLine("Process payment");
+            return true;
+        }
+    }
+
+    public class OrderSystemProxy : IOrderSystem
+    {
+        public string CurrentUser = string.Empty;
+        private OrderSystem OrderSystem = null;
+
+        public OrderSystemProxy(string user)
+        {
+            CurrentUser = user;
+            OrderSystem = new OrderSystem();
+        }
+
+        public bool MakeOrder()
+        {
+            return OrderSystem.MakeOrder();
+        }
+
+        public bool MakePayment()
+        {
+            if (CurrentUser == "Intern")
+            {
+                Console.WriteLine("Intern is not allow to make payment");
+                return false;
+            }
+            else
+            {
+                return OrderSystem.MakePayment();
+            }
+        }
+    }
+}
---
+++ PatternsExamplesTest/Structural/ProxyTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using PatternsExamples.Structural.Proxy;
+
+namespace PatternsExamplesTest.Structural
+{
+    [TestFixture]
+    public class ProxyTest
+    {
+        [Test]
+        public void InternShouldAllowedToMakeOrder()
+        {
+            var proxy = new OrderSystemProxy("Intern");
+            Assert.IsTrue(proxy.MakeOrder());
+        }
+
+        [Test]
+        public void InternShouldNotAllowedToMakePayment()
+        {
+            var proxy = new OrderSystemProxy("Intern");
+            Assert.IsFalse(proxy.MakePayment());
+        }
+    }
+}
